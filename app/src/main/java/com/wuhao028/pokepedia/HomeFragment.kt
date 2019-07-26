fe84c3c7e1c35ac0c863ba4bcaceef8852986bc9
wuhao028 <wuhao028@gmail.com>
2019-07-26 21:01:07 +1200

    add framework

---
+++ app/src/main/java/com/wuhao028/pokepedia/HomeFragment.kt
@@ -0,0 +1,7 @@
+package com.wuhao028.pokepedia
+
+import androidx.fragment.app.Fragment
+
+class HomeFragment : Fragment() {
+
+}
--- app/src/main/java/com/wuhao028/pokepedia/MainActivity.kt
+++ app/src/main/java/com/wuhao028/pokepedia/MainActivity.kt
@@ -2,11 +2,24 @@ package com.wuhao028.pokepedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.wuhao028.pokepedia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this,
+            R.layout.activity_main)
+        navController = findNavController(R.id.pokemon_nav_fragment)
+
+
     }
 }
